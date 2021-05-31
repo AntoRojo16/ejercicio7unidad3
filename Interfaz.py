2714dc56b6d5f3fa174209ee4e358e7e22f3b937
AntoRojo16 <antorojo1604@gmail.com>
2021-05-30 22:49:03 -0300

    mi primer commit

---
+++ Interfaz.py
@@ -0,0 +1,12 @@
+from zope.interface import Interface
+
+class IColeccion(Interface):
+    
+    def InsertarAgente(posicion,elemento):
+        pass
+    
+    def AgregarAgente(elemento):
+        pass
+    
+    def MostrarAgente(posicion):
+        pass
